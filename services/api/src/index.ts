ab050d190b6f65024e52d012fe274c2f4373287f
Javier Saiz <javiersaizcano@gmail.com>
2021-11-06 10:58:09 +0100

    add /currency routes

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,7 +1,6 @@
-import * as path from 'path';
-import * as express from 'express';
-import * as mongoose from 'mongoose';
-
+import * as path from "path";
+import * as express from "express";
+import * as mongoose from "mongoose";
 
 // [DB Connection]
 
@@ -10,7 +9,7 @@ declare var MONGODB_URI: string;
 /**
  * connectToDatabase
  * Configures the global MongoDB connection based on the provided secrets.
- * 
+ *
  * @returns Promise<string>
  */
 async function connectToDatabase(connectionUri: string) {
@@ -18,32 +17,47 @@ async function connectToDatabase(connectionUri: string) {
     // From mongoose@6.x.x onwards useNewUrlParser, useUnifiedTopology,
     // useCreateIndex are deprecated and default to true
     mongoose
-    .connect(connectionUri)
-      .then(() =>
-        resolve(connectionUri)
-      )
+      .connect("mongodb://home-assignment-db:27017/testdb")
+      .then(() => resolve("mongodb://home-assignment-db:27017/testdb"))
       .catch((error: any) => {
-        console.log(error)
-        reject(`${connectionUri}: ${error}`)
+        console.log(error);
+        reject(`${"mongodb://home-assignment-db:27017/testdb"}: ${error}`);
       });
-  })
+  });
 }
 connectToDatabase(MONGODB_URI);
 
 // [Express setup]
 
-const app = express(), DIST_DIR = __dirname;
+const app = express(),
+  DIST_DIR = __dirname;
 app.use(express.static(DIST_DIR));
 app.use(express.json());
 
-app.get('/health', async (req, res) => {
-  res.status(200).json({healthy: true});
+app.get("/health", async (req, res) => {
+  res.status(200).json({ healthy: true });
+});
+
+app.get("/currency", async (req, res) => {
+  res.status(200).json({ healthy: true });
+});
+
+app.post("/currency", async (req, res) => {
+  res.status(201).send(req.body);
+});
+
+app.delete("/currency", async (req, res) => {
+  res.status(201).json({ healthy: true });
+});
+
+app.use((req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
 });
 
 // [Express start]
 
-const PORT: number | string = process.env.PORT || 8080
+const PORT: number | string = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`)
-  console.log('Press Ctrl+C to quit.')
-})
+  console.log(`App listening to ${PORT}....`);
+  console.log("Press Ctrl+C to quit.");
+});
